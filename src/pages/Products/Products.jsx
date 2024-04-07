b29c62416d3ab7e7a89487e45bcc76ac462a8b8e
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2024-04-08 02:11:37 +0600

    add to cart button added in the product page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import SectionTitle from "../../components/SectionTitle";
+import cartIcon from "../../assets/Image/cart-icon.png"; 
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -20,6 +22,7 @@ const Products = () => {
   return (
     <>
       <section className="text-gray-600 body-font">
+        <SectionTitle heading="All Products" />
         <div className="container px-5 py-24 mx-auto">
           <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 w-full text-center">
             {products.map((item, index) => (
@@ -38,6 +41,26 @@ const Products = () => {
                     {item.title}
                   </h2>
                   <p className="mt-1">${item.price}</p>
+
+                  <span className="text-orange-500 ml-3 font-semibold">
+                    {item.rating.rate} Reviews
+                  </span>
+                  <button className="flex items-center w-full justify-center mx-auto mt-3 gap-2 px-3 py-1.5 text-sm text-black duration-150 bg-slate-400 rounded-lg hover:bg-slate-500 hover:text-white ">
+                    {/* <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
+                        clipRule="evenodd"
+                      />
+                    </svg> */}
+                    <img src={cartIcon} className="w-8 h-8" alt="cart icon" />
+                    Add to Cart
+                  </button>
                 </div>
               </div>
             ))}
